243daf48d988209b40cf387b62e76595c2b46da7
Gaurav Bhatt <07gauravbhatt@gmail.com>
2018-02-11 22:36:33 +0530

    Update SparseArrays.java

--- Data Structure/SparseArrays.java
+++ Data Structure/SparseArrays.java
@@ -2,29 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class SparseArrays {
-
     public static void main(String[] args) {
         Scanner sn= new Scanner(System.in);
         int slength=sn.nextInt();
         String sarr[]=new String[slength];
-        for(int i=0;i<slength;i++)
-        {
+        for(int i=0;i<slength;i++){
             sarr[i]=sn.next();
         }
         int queries=sn.nextInt();
         String qarr[]=new String[queries];
-        for(int j=0;j<queries; j++)
-        {
+        for(int j=0;j<queries; j++){
             qarr[j]=sn.next();
         }
 
-        for(int k=0;k<qarr.length;k++)
-        {
+        for(int k=0;k<qarr.length;k++){
             int count=0;
             String tmp=qarr[k];
 
-            for(int x=0;x<sarr.length;x++)
-            {
+            for(int x=0;x<sarr.length;x++){
                 if(sarr[x].equals(tmp))
                 count++;
             }
